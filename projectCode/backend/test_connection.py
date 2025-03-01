9194e01db0c4f5c9c3bd741ba94b96e4aec25de5
WillowZuvich <willow.zuvich@wsu.edu>
2025-03-01 14:33:33 -0800

    Fixed environment variable grab

--- projectCode/backend/test_connection.py
+++ projectCode/backend/test_connection.py
@@ -1,8 +1,12 @@
 # This tile ensure FastAPI can successfully connect to MySQL
 import sqlalchemy
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Load environment variables from .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 try:
     engine = sqlalchemy.create_engine(DATABASE_URL)
