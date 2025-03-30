215b955be669758cb00d448173cc57c3110d53d7
WillowZuvich <willow.zuvich@wsu.edu>
2025-03-30 12:29:25 -0700

    Completed remove equipment

--- projectCode/frontend/src/App.js
+++ projectCode/frontend/src/App.js
@@ -74,9 +74,6 @@ const AppContent = () => {
           <Navbar />
           <Footer />
         </>
-<<<<<<< HEAD
-        
-=======
       )}    */}
 
       <>
@@ -85,7 +82,6 @@ const AppContent = () => {
       </>  
 
 
->>>>>>> c241243 (Equipment & Restructure of the user pages)
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<HeroSection />} />
@@ -102,6 +98,9 @@ const AppContent = () => {
         <Route path="/editequipment" element={<ProtectedRoute allowedRoles={["admin"]}><EditEquipment /></ProtectedRoute>} />
         <Route path="/addsupplier" element={<ProtectedRoute allowedRoles={["admin"]}><AddSupplier /></ProtectedRoute>} />
         <Route path="/addspecifications" element={<ProtectedRoute allowedRoles={["admin"]}><AddSpecifications /></ProtectedRoute>} /> */}
+
+        <Route path="/removeequipment" element={<ProtectedRoute allowedRoles={["admin"]}><RemoveEquipment /></ProtectedRoute>} />
+        <Route path="/editequipment" element={<ProtectedRoute allowedRoles={["admin"]}><EditEquipment /></ProtectedRoute>} />
 
         {/* === Admin Routes (each one inside AdminLayout) === */}
         <Route path="/admin" element={<ProtectedRoute allowedRoles={['admin']}><AdminLayout><AdminDashboard  /></AdminLayout></ProtectedRoute>} />
