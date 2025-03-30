4e9d49cccd8e30f9f75cc4d6965ce5f2b6cdcfa3
WillowZuvich <willow.zuvich@wsu.edu>
2025-03-29 18:35:07 -0700

    Completed process for adding equipment specifications.

--- projectCode/frontend/src/Specifications/AddSpecifications.jsx
+++ projectCode/frontend/src/Specifications/AddSpecifications.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './AddSpecification.css';
 
 const AddSpecifications = () => {
   const [inputs, setInputs] = useState(['']);
 
   const MAXSPEC = 7
+
+  const location = useLocation();
+  const equip = location.state;
 
   const addInputField = () => {
     if (inputs.length < MAXSPEC) {
@@ -23,36 +27,55 @@ const AddSpecifications = () => {
     setInputs(updatedInputs); 
   };
 
-/*   const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const apiUrl = process.env.REACT_APP_API_URL;  // Connect to backend
-    try {
-      const response = await fetch(`${apiUrl}/api/addspecification`, { 
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name: formData.name.trim(), 
-          condition: formData.condition,
-          supplierName: formData.supplierName.toLowerCase(), // Convert all supplier names to lower case for ease of user experience.
-        }),
-      });
-  
-      const data = await response.json();
-      if (response.ok) {
-        alert('Specifications added succesfully. Please add equipment details.');
-        // TODO: Finish add specification page
-      } else {
-        alert(data.detail);
+    for(var i = 0; i < inputs.length; i++){
+      
+      if (!equip.EquipID ) {
+        alert("equipId Missing.");
+        return;
       }
-    } catch (error) {
-      console.error('Unable to add item:', error);
-      alert('Something went wrong. Please try again.');
+      
+      const item = inputs[i];
+
+      if ( !item) {
+        alert("Specification is Missing.");
+        return;
+      }
+      else{ console.log(JSON.stringify({ 
+        equipId: equip.EquipID, 
+        input: item,
+      }),)}
+      try {
+        const response = await fetch(`${apiUrl}/api/addspecifications`, { 
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ 
+            equipId: equip.EquipID[0], 
+            input: item,
+          }),
+        });
+    
+        const data = await response.json();
+        if (response.ok) {
+        } else {
+          alert(data.detail);
+        }
+      } catch (error) {
+        console.error('Unable to add details:', error);
+        alert('Something went wrong. Please try again.');
+      
+      }
     }
-  }; */
+    alert('Specifications added succesfully.');
+    navigate("/admin")
+  };
 
   return (
-      <div  className="wrapper-addSpec">
+      <div  className="wrapper-addSpec" >
         <div className="title-addSpec">Add Specifications</div>
+        <div className="title-addSpec">For Item "{equip.Name}", {equip.EquipID}</div>
         <div className="field">
           <button className = "field" onClick={addInputField}>Add Additional Specification</button>
           <div className="field">
@@ -68,9 +91,11 @@ const AddSpecifications = () => {
               </div>
             ))}
           </div>
-          <div >
-            <input type="submit" value="Submit" className="add-btn" />
-          </div>
+          <form onSubmit={handleSubmit} className="form-addEquip">
+            <div >
+              <input type="submit" value="Submit" className="add-btn" />
+            </div>
+          </form>
         </div>
       </div>
   );
