60e74fe270364717d8ceb0d07a3f86362dc3d73d
WillowZuvich <willow.zuvich@wsu.edu>
2025-04-12 14:02:16 -0700

    completed unit tests

--- projectCode/backend/test_auth.py
+++ projectCode/backend/test_auth.py
@@ -5,6 +5,7 @@ from fastapi import status
 from auth import app
 from database import SessionLocal, Base, engine
 from models import Supplier, Equipment, Specification, Admin, Booking, Student, Researcher
+import requests
 
 # Create test database schema
 Base.metadata.create_all(bind=engine)
@@ -15,8 +16,8 @@ def test_db():
     try:
         yield db
     finally:
-        db.close()
-
+"""         db.close()
+""" 
 @pytest.mark.asyncio
 async def test_register_user():
     db = SessionLocal()
@@ -247,16 +248,14 @@ async def test_add_supplier(test_db):
         response = await ac.post("/api/addsupplier", json=payload)
         assert response.status_code == 200
         assert response.json()["message"] == "Supplier added successfully!"
-
+""" """
 @pytest.mark.asyncio
 async def test_get_student_booking(test_db):
     test_db.rollback()
-    student = test_db.query(Student).filter(Student.StudentID == 19).first()
-    if not student:
-        pytest.skip("No student matching ID=19.")
-
+    student = test_db.query(Student).first()
+ 
     payload = {
-        "student_id" : "19"
+        "student_id" : student.StudentID
     }
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
@@ -266,15 +265,67 @@ async def test_get_student_booking(test_db):
 @pytest.mark.asyncio
 async def test_get_researcher_booking(test_db):
     test_db.rollback()
-    researcher = test_db.query(Researcher).filter(Researcher.EmpID == 3).first()
-    if not researcher:
-        pytest.skip("No Researcher matching ID=3.")
-
+    researcher = test_db.query(Researcher).first()
     payload = {
-        "researcher_id" : "3"
+        "researcher_id" : researcher.EmpID
     }
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/api/researcher/bookings", params=payload)
         assert response.status_code == 200
 
+@pytest.mark.asyncio
+async def test_get_all_bookings(test_db):
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        response = await ac.get("/api/admin/bookings")
+        assert response.status_code == 200 
+
+@pytest.mark.asyncio
+async def test_create_booking(test_db):
+    test_db.rollback()
+    equipid = 1
+    booked = True
+    equip = test_db.query(Equipment).filter(Equipment.EquipID == equipid).first()
+    while booked and equipid < 50:
+        equip = test_db.query(Equipment).filter(Equipment.EquipID == equipid).first()
+        if equip:
+            booking = test_db.query(Booking).filter(Booking.EquipmentID == equip.EquipID).first()
+        equipid += 1
+        if equip and not booking:
+            booked = False
+
+    if not equip:
+        pytest.skip("No items available to book")
+
+    user = test_db.query(Student).first()
+
+    payload ={
+        "user_id" : user.StudentID,
+        "equipment_id" : equip.EquipID,
+        "user_type" : "student"
+    }
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        response = await ac.post("/api/bookings", json = payload)
+        assert response.status_code == 200
+        assert response.json()["message"] == "Booking request created successfully"
+
+@pytest.mark.asyncio
+async def test_update_booking_status(test_db):
+    booking = test_db.query(Booking).filter(Booking.Status == "Pending Request").first()
+    if not booking:
+        pytest.skip("No pending requests.")
+    id = booking.BookingID
+    newStatus = "Approved"
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        headers = {'Content-type': 'application/json'}
+        addr = f"/api/bookings/{id}/status"
+        payload = {
+            "status": newStatus,
+            "admin_action": "Approved by admin"
+        }
+        response = await ac.put(addr, json =payload)
+        assert response.status_code == 200
+        assert response.json()["message"] == f"Booking status updated to Approved"
